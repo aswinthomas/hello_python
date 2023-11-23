f9bb5ccc3cf20748a52fe36fcfcd618be713cd65
aswinthomas <aswinthomas@gmail.com>
2023-11-23 10:20:45 +0800

    updated class methods

--- methods.py
+++ methods.py
@@ -1,32 +1,55 @@
+import csv
+
+
 class Item:
-    # Class attribute. Pay rate after 20% discount
-    pay_rate = 0.8
-    all = []
+	# Class attribute. Pay rate after 20% discount
+	pay_rate = 0.8
+	all = []
 
-    # Constructor. passing types is not necessary here due to default assignment
-    def __init__(self, name="test", price=1., quantity=1):
-        # Run validations for arguments
-        assert price > 0, f"Price {price} is not greater than zero"
-        assert quantity > 0, f"Quantity {quantity} is not greater than zero"
+	# Constructor. passing types is not necessary here due to default assignment
+	def __init__(self, name="test", price=1., quantity=1):
+		# Run validations for arguments
+		assert price > 0, f"Price {price} is not greater than zero"
+		assert quantity > 0, f"Quantity {quantity} is not greater than zero"
 
-        # Assign to self object
-        self.name = name
-        self.price = price
-        self.quantity = quantity
-        print(f"Constructor called with name={self.name} price={self.price} quantity={self.quantity}")
+		# Assign to self object
+		self.name = name
+		self.price = price
+		self.quantity = quantity
+		print(f"Constructor called with name={self.name} price={self.price} quantity={self.quantity}")
 
-        # Execution
-        Item.all.append(self)
+		# Execution
+		Item.all.append(self)
 
-    def calculate_total_price(self) -> float:
-        return self.price * self.quantity
+	def calculate_total_price(self) -> float:
+		return self.price * self.quantity
 
-    def apply_discount(self):
-        self.price = self.price * self.pay_rate
+	def apply_discount(self):
+		self.price = self.price * self.pay_rate
 
-    @classmethod
-    def instantiate_from_csv(cls):
+	@classmethod
+	# Use class method when you want a method to manipulate data structures e.g. instantiate objects
+	def instantiate_from_csv(cls):
+		with open('items.csv', 'r') as f:
+			reader = csv.DictReader(f)
+			items = list(reader)
 
-    # Beautify return of instance
-    def __repr__(self):
-        return f"Item('{self.name}', {self.price}, {self.quantity})"
+		for item in items:
+			Item(name=item.get('name'),
+			     price=float(item.get('price')),
+			     quantity=int(item.get('quantity')))
+
+	@staticmethod
+	# Use static method when it doesnt need to be unique per instance
+	def is_integer(num):
+		# Count the floats are in form .0 e.g. 5.0, 10.0
+		if isinstance(num, float):
+			return num.is_integer()
+		elif isinstance(num, int):
+			return True
+		else:
+			return False
+
+	# Beautify return of instance
+	def __repr__(self):
+		return f"Item('{self.name}', {self.price}, {self.quantity})"
--- methods_test.py
+++ methods_test.py
@@ -1,4 +1,19 @@
 import methods
 
+
 def test_method():
-    Item.instantiate_from_csv()
+	methods.Item.instantiate_from_csv()
+	assert any(item.name == 'Laptop' and item.quantity == 3 and item.price == 1000.0 for item in methods.Item.all)
+	# Although using an instance is not incorrect, there is no reason to do this
+	digital_items = methods.Item()
+	digital_items.instantiate_from_csv()
+	assert any(item.name == 'Laptop' and item.quantity == 3 and item.price == 1000.0 for item in digital_items.all)
+
+
+def test_is_integer():
+	assert methods.Item.is_integer(243) is True
+	assert methods.Item.is_integer(34.0) is True
+	assert methods.Item.is_integer(10.3) is False
+	# Although using an instance is not incorrect, there is no reason to do this
+	digital_items = methods.Item()
+	assert digital_items.is_integer(243) is True
